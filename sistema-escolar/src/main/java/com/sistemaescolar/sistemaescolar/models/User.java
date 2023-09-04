04635bda9546997d1443fdc82542a78dbdcb8b80
NewtonAmbrizzi <newton.ambrizzi@gmail.com>
2023-09-04 15:33:13 -0300

    Model e Repository user

--- sistema-escolar/src/main/java/com/sistemaescolar/sistemaescolar/models/User.java
+++ sistema-escolar/src/main/java/com/sistemaescolar/sistemaescolar/models/User.java
@@ -1,12 +1,83 @@
 package com.sistemaescolar.sistemaescolar.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "users")
 public class User {
 
-    private String nome;
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable=false, length=150)
+    private String name;
+
+    @Column(nullable=false, length=150)
+    private String email;
+
+    @Column(nullable=false, length=100)
+    private String type;
+    
+    @Column(nullable=false, length=255)
+    private String password;
+
+    @Column(nullable=false, length=30)
+    private String status;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    
     
 }
--- sistema-escolar/src/main/java/com/sistemaescolar/sistemaescolar/repositories/UsersRepository.java
+++ sistema-escolar/src/main/java/com/sistemaescolar/sistemaescolar/repositories/UsersRepository.java
@@ -1,6 +1,8 @@
 package com.sistemaescolar.sistemaescolar.repositories;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.sistemaescolar.sistemaescolar.models.User;
 
 public interface UsersRepository extends JpaRepository<User, Integer> {
     
