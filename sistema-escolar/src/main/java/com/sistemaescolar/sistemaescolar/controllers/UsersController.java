fff69026257e159fabd1bde672500b6bc8430449
NewtonAmbrizzi <newton.ambrizzi@gmail.com>
2023-09-05 11:52:53 -0300

    Criação de página de cadastro de usuários

--- sistema-escolar/src/main/java/com/sistemaescolar/sistemaescolar/controllers/UsersController.java
+++ sistema-escolar/src/main/java/com/sistemaescolar/sistemaescolar/controllers/UsersController.java
@@ -1,16 +1,32 @@
 package com.sistemaescolar.sistemaescolar.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.sistemaescolar.sistemaescolar.models.User;
+import com.sistemaescolar.sistemaescolar.repositories.UsersRepository;
+
 @Controller
 public class UsersController {
 
+    @Autowired
+    private UsersRepository repository;
+
     @GetMapping(value = {"/users"})
     public String index(Model model){
-        model.addAttribute("nome", "Newton");
+        List<User> users = repository.findAll();
+        model.addAttribute("users", users);
         return "users/index";
+    }
+
+    @GetMapping(value = {"/users/new-user"})
+    public String newUser(Model model){
+        
+        return "users/new-user";
     }
     
 }
--- sistema-escolar/src/main/java/com/sistemaescolar/sistemaescolar/repositories/UsersRepository.java
+++ sistema-escolar/src/main/java/com/sistemaescolar/sistemaescolar/repositories/UsersRepository.java
@@ -5,5 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.sistemaescolar.sistemaescolar.models.User;
 
 public interface UsersRepository extends JpaRepository<User, Integer> {
+
     
 }
